59417d6f0b756249cb7d1bbd9df24f22a29cf11b
Alex Murray <alexmurraydesign@gmail.com>
2022-01-12 23:01:38 +1300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,66 +12,74 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-text-white-ter">
-        <div className="content has-text-centered has-text-white-ter">
-            <div style={{ maxWidth: "100vw" }} className="columns">
-              <div className="column is-6">
-                <section>
-                  <img
-                    src={logo}
-                    alt="Sean McAnally"
-                    style={{ width: "14em", height: "5em" }}
-                  />
-                </section>
-              </div>
-              <div className="column is-6 social">
-                {/* <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a> */}
-                <a title="linkedin" href="https://linkedin.com">
-                  <img
-                    src={linkedin}
-                    alt="linkedin"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <ul className="contact-list">
-                    <li>
-                      <a href="tel:0274891235" className="navbar-item">
-                        027 489 1235
+        <div className="container">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="columns">
+                  <div className="column is-6 footer-logo">
+                    <section>
+                      <Link to="/">
+                        <img
+                          src={logo}
+                          alt="Sean McAnally"
+                          style={{ width: "16em", height: "5em" }}
+                        />
+                      </Link>
+                    </section>
+                  </div>
+                  <div className="column is-6">
+                    {/* <a title="facebook" href="https://facebook.com">
+                      <img
+                        src={facebook}
+                        alt="Facebook"
+                        style={{ width: "1em", height: "1em" }}
+                      />
+                    </a>
+                    <a title="twitter" href="https://twitter.com">
+                      <img
+                        className="fas fa-lg"
+                        src={twitter}
+                        alt="Twitter"
+                        style={{ width: "1em", height: "1em" }}
+                      />
+                    </a>
+                    <a title="instagram" href="https://instagram.com">
+                      <img
+                        src={instagram}
+                        alt="Instagram"
+                        style={{ width: "1em", height: "1em" }}
+                      />
+                    </a>
+                    <a title="vimeo" href="https://vimeo.com">
+                      <img
+                        src={vimeo}
+                        alt="Vimeo"
+                        style={{ width: "1em", height: "1em" }}
+                      />
+                    </a> */}
+                    <div className="social">
+                      <a title="linkedin" href="https://linkedin.com">
+                        <img
+                          src={linkedin}
+                          alt="linkedin"
+                          style={{ width: "1em", height: "1em" }}
+                        />
                       </a>
-                    </li>
-                    <li>
-                      <a href="mailto:sean@seanmcanally.co.nz" className="navbar-item">
-                        sean@seanmcanally.co.nz
-                      </a>
-                    </li>
-                </ul>
+                    </div>
+                    <ul className="contact-list">
+                        <li>
+                          <a href="tel:0274891235">
+                            027 489 1235
+                          </a>
+                        </li>
+                        <li>
+                          <a href="mailto:sean@seanmcanally.co.nz">
+                            sean@seanmcanally.co.nz
+                          </a>
+                        </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
